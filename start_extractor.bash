b31c5edcfd70bd90e82cec26afffbae882785961
Reinholds Zviedris <reinholds@zviedris.lv>
2023-07-17 19:58:56 +0300

    updated docker image references to ghcr.io

--- start_extractor.bash
+++ start_extractor.bash
@@ -4,4 +4,4 @@ docker run --name extractor -d \
   -v /opt/traefik:/app/data \
   -v ${PWD}/certs:/app/certs \
   -v /var/run/docker.socket:/var/run/docker.socket \
-  estivadorio/traefik-certificate-extractor
+  ghcr.io/estivador/traefik-certificate-extractor
